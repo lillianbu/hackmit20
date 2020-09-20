0e5e6395594274e3cd8016748f51886f2f05743b
cphung <cphung@mit.edu>
2020-09-20 05:33:57 -0400

    left right on carousel

--- src/CandidateCard.js
+++ src/CandidateCard.js
@@ -17,8 +17,10 @@ export default class CandidateCard extends Component {
         <div className="break"></div>
         <div className="topic-container">
           <p className="topic-title">average position</p>
-          <div className="avg-position-slider-container">
+            <div className="avg-lean-container">
+            <a>left</a>
             <Slider candLeaning={this.props.avgLean}></Slider>
+            <a>right</a>
           </div>
         </div>
         <div className="select-text">
