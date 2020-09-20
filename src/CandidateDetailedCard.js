a020d0e17a8b20a7abeac284ee5d2e5818242c45
cphung <cphung@mit.edu>
2020-09-20 04:04:12 -0400

    removed left right

--- src/CandidateDetailedCard.js
+++ src/CandidateDetailedCard.js
@@ -22,12 +22,12 @@ export default class CandidateDetailedCard extends Component {
           </div>
           <div className="break"></div>
           <a>average position</a>
-          <Slider candLeaning={this.props.avgLean}></Slider>
           <a>left</a>
+          <Slider candLeaning={this.props.avgLean}></Slider>
           {/* <Slider candLeaning={this.props.candLeaning} detailedCard={true}></Slider> */}
-          {console.log(this.props.allQuestions)}
-          {this.props.leanSliders}
+          {/* {console.log(this.props.allQuestions)} */}
           <a>right</a>
+          {this.props.leanSliders}
           <div className="break"></div>
         </div>
         <div className="vl"></div>
